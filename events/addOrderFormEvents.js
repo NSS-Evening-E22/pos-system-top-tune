78cb538dfabd5b7f4ec043f6ae56a0e02e709ad8
coleama <cole.ama@gmail.com>
2023-03-16 19:04:23 -0500

    correcting events

--- events/addOrderFormEvents.js
+++ events/addOrderFormEvents.js
@@ -1,16 +1,19 @@
 import { getSingleOrder } from '../api/orderData';
 import addOrderForm from '../components/forms/addOrderForm';
 
-// TODO: CLICK EVENT FOR SHOWING FORM FOR ADDING A BOOK
-const addOrderFormEvent = () => {
-  if (e.target.id.includes('update-order')) {
+const addOrderformEvents = () => {
+  document.querySelector('#main-container').addEventListener('submit', (e) => {
+    e.preventDefault();
+    // EVENT FOR UPDATING ORDER
+    if (e.target.id.includes('update-order')) {
       console.warn('update order');
       if (e.target.id.split('--')) {
         const [, firebaseKey] = e.target.id.split('--');
 
-        getSingleOrder(firebaseKey).then((entryObj) => addOrderForm(user.uid, entryObj));
+        getSingleOrder(firebaseKey).then((entryObj) => addOrderForm(entryObj));
       }
     }
+  });
 };
 
-export default addOrderFormEvent;
+export default addOrderformEvents;
